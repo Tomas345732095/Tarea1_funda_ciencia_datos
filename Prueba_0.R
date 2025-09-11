16b9737fdf33cfd5395f7ab52303c1f6781a618c
Tomas345732095 <jolezaeta@alumnos.uai.cl>
2025-09-11 16:56:12 -0300

    Update Prueba_0.R

--- Prueba_0.R
+++ Prueba_0.R
@@ -60,6 +60,66 @@ boxplot(datos[19])
 boxplot (c (unlist(datos[19]),unlist(datos[20]),unlist(datos[21])))
 
 
+#pregunta 7 
+
+#Vista de los datos por separado
+boxplot(datos[,19:20])#Forma alternativa: boxplot(datos$AE_PJE,datos$EP_PJE)
+summary(datos$AE_PJE)
+summary(datos$EP_PJE)
+table(datos$AE_PJE)
+table(datos$EP_PJE)
+
+#Vista unida de los datos 
+boxplot (c (unlist(datos[,19]),unlist(datos[,20])))#Forma alternativa: boxplot (c (datos$AE_PJE,datos$EP_PJE))
+summary(c (unlist(datos[,19]),unlist(datos[,20])))
+table(c (unlist(datos[,19]),unlist(datos[,20])))
+
+
+######pregunta 8
+
+#matrix(c(summary(datos[2])),c(summary(datos[3]))
+summary(datos[,5])
+data.frame(summary(datos[,3]))
+summary(datos[2])
+summary(datos[,3])
+a<-(summary(datos[,3]))
+matrix(summary(datos[,3]))
+
+
+
+Datos<-c("Perc.25","Perc.50","Perc.75","Moda","Media")
+a<-
+
+#Pregunta 9
+
+b<-c(datos[,3])
+#c<-datos[2]
+View(b)
+print(c(datos[,2]))
+#a<-datos$nombre
+View(cor(b))
+
+View(cor(datos[,1],datos[,2],datos[,3],datos[,5],datos[,6],datos[,7],datos[,8],datos[,10],datos[,11],datos[,12],datos[,13],datos[,14],datos[,16],datos[,17]))
+View(cor(datos[,1],datos[,2],datos[3]))
+View(cor(datos[,1],datos[,2],datos[,3],datos[,5]))
+
+a<-datos[,3]
+b<-datos[,2]
+c<-datos[,1]
+
+cor(a,b,c)
+
+
+
+tabla_fin<-datos %>%
+  filter(NOM_DEPROV_RBD =="BIOBIO"|NOM_DEPROV_RBD =="ÑUBLE")
+
+tabla_fin_uno<-tabla_fin %>%
+  filter()
+print(tabla_fin)
+View(tabla_fin)
+
+
 
 
 
